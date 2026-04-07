c17bc930789518bcc4e0fa2c654c4af92b911932
Barun Ghorai <ranghorai865@gmail.com>
2026-04-08 01:09:33 +0530

    Add new user to db.json

--- src/app/_components/RegisterForm.tsx
+++ src/app/_components/RegisterForm.tsx
@@ -1,44 +1,49 @@
 'use client'
 
 import React from 'react'
+import { registerAction } from '../actions/auth'
 
 const RegisterPage = () => {
-  return (
-            <form
-                className='space-x-4'>
-                <div>
-                    <label className='block text-sm font-bold text-gray-800'>User</label>
-                    <input type="text"
-                        name="username"
-                        placeholder='Give your name'
-                        required
-                        className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
-                    />
-                </div>
-                <div>
-                    <label className='block text-sm font-bold text-gray-800'>Email</label>
-                    <input type="email"
-                        name="email"
-                        placeholder='Give your email'
-                        required
-                        className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
-                    />
-                </div>
-                <div>
-                    <label className='block text-sm font-bold text-gray-800'>Password</label>
-                    <input type="password"
-                        name="password"
-                        placeholder='Give your password'
-                        required
-                        className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full mt-4 flex justify-center py-2 px-4 border border-transparent 700 bg-blue-500 rounded-2xl"
-                >Register</button>
-            </form>
-  )
+    return (
+        <form
+            action={ registerAction }
+            className='space-x-4'>
+            <div>
+                <label className='block text-sm font-bold text-gray-800'>User</label>
+                <input type="text"
+                    name="name"
+                    placeholder='Give your name'
+                    required
+                    autoComplete="off"
+                    className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
+                />
+            </div>
+            <div>
+                <label className='block text-sm font-bold text-gray-800'>Email</label>
+                <input type="email"
+                    name="email"
+                    placeholder='Give your email'
+                    required
+                    autoComplete="off"
+                    className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
+                />
+            </div>
+            <div>
+                <label className='block text-sm font-bold text-gray-800'>Password</label>
+                <input type="password"
+                    name="password"
+                    placeholder='Give your password'
+                    required
+                    autoComplete="off"
+                    className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2'
+                />
+            </div>
+            <button
+                type="submit"
+                className="w-full mt-4 flex justify-center py-2 px-4 border border-transparent 700 bg-blue-500 rounded-2xl"
+            >Register</button>
+        </form>
+    )
 }
 
 export default RegisterPage
--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -34,6 +34,41 @@ export const loginAction = async (formData: FormData) => {
 
 };
 
+export const registerAction = async (formData: FormData) => {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const nameStr = name?.toString().trim() || "";
+  const emailStr = email?.toString().trim() || "";
+  const passwordStr = password?.toString().trim() || "";
+
+  if(!nameStr || !emailStr || !passwordStr){
+    throw new Error("Fill All the fields");
+  }
+
+  //Checking user is exist or not
+  const response = await axios.get(`${API_URL}/users`);
+  const existingUser = response.data.find( 
+    (u:any) => u.email === emailStr
+  )
+
+  if(existingUser){
+    throw new Error("User is already Exists");
+  }
+
+  //Creating new User
+  const newUser = {
+    name: nameStr,
+    email: emailStr,
+    password: passwordStr
+  }
+  
+  const res = await axios.post(`${API_URL}/users`, newUser);
+  console.log("New user Registered",res.data);
+  redirect("/login");
+}
+
 
 export const logoutAction = async () => {
   await deleteSession();
