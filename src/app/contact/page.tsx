a397bd81bbd318880f668fb9a650c2c58acf4a1f
Barun Ghorai <ranghorai865@gmail.com>
2026-04-08 16:08:18 +0530

    Creating contact page for new contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { getSession } from '../_lib/session';
 import { redirect } from 'next/navigation';
+import { getContacts } from '../api/contact';
 
 const Contactpage = async () => {
-const userId = await getSession();
-if(!userId) {
-  redirect("/login");
-}
+  const userId = await getSession();
+
+  if (!userId) {
+    redirect("/login");
+  }
+  const contacts = await getContacts(userId)
+  console.log("cont:",contacts);
+  
+  if (!contacts || contacts.length === 0) {
+    return (
+      <div>
+        please{" "}
+        <a href="/contact/new" className='text-blue-600 hover:underline'>
+          Add a contact
+        </a> {" "}
+        to your contact list
+      </div>
+    )
+  }
   return (
     <div>ContactPage</div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div className="flex flex-col items-center justify-items-center min-h-screen">
+      <div className="text-center">
+        <h2>Welcome to Contact Manager</h2>
+        <p className="mt-2 text-lg text-gray-600">
+          Manage yopur contact easily and efficiently
+        </p>
+        <Image
+          src="/0014.png"
+          alt="contact manager"
+          width={300}
+          height={300}
+          className="rounded-lg shadow-lg"
+        />
+      </div>
+      <div className="text-center">
+        <p className="mt-2 text-lg text-gray-600">
+          start managing your contact today
+        </p>
+      </div>
+    </div>
   );
 }
